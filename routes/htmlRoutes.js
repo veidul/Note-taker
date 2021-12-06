b93af3f585117f258940ea08be6fb3257e651867
ludie lambright <ludiefletcher@aol.com>
2021-12-05 22:31:43 -0800

    added module exports function to htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,13 +3,12 @@ var path = require('path');
 const dbPath = path.join(__dirname, "../db/db.json");
 const { v4: uuidv4 } = require('uuid');
 
-
-
-
-
-
-
-
-
-
-module.exports = 
+module.exports = function(app) {
+    app.get('/api/notes', (req,res) => {
+        fs.readFile(dbPath, 'utf8', (err, data)=>{
+            if (err) throw err;
+            console.log(data);
+            res.json(data)
+        })
+    })
+}
