21c8b86ac8214859759e233c691d173a742bd34b
ludie lambright <ludiefletcher@aol.com>
2021-12-05 22:40:53 -0800

    wrote out app.get function for htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,3 +1,4 @@
+const { json } = require('express');
 const fs = require('fs');
 var path = require('path');
 const dbPath = path.join(__dirname, "../db/db.json");
@@ -12,3 +13,26 @@ module.exports = function(app) {
         })
     })
 }
+
+app.post("api/notes", (req,res) => {
+    fs.readFile(dbPath, 'utf8', (err, data)=>{
+        if(err) throw err;
+        let notesList;
+        if (data) {
+            notesList = JSON.parse(data);
+        }
+        let newNote = req.body;
+        newNote.id = uuidv4();
+        if (notesList) {
+            notesList.push(newNote);
+        }
+        else {
+            notesList = [newNote]
+        }
+        fs.writeFile(dbPath, JSON.stringify(notesList), (err,data) => {
+            if (err) throw err;
+            console.log('Success you have created a new note! ID: ' +newNote.id);
+            res.json(newNote)
+        })
+    })
+})
